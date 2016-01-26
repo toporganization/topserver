de2de30f785003e57e689a53bee7a2951ba94b30
张植臻 <zhizhen@MacAir.lan>
2016-01-26 08:25:30 +0800

    topserver sql files

--- topserver/urls.py
+++ topserver/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from api import views as learn_views  # new
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+        url(r'$', learn_views.index),
+        url(r'^admin/', admin.site.urls),
 ]
